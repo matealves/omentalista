066c3cb33f4650fc76b23397545a9a0eb1cc1334
Mateus <contatomateusalves@hotmail.com>
2022-03-09 20:33:50 -0300

    Update 1.3

--- app.js
+++ app.js
@@ -28,16 +28,17 @@ function Chutar() {
     } else if (chute >10 || chute <0) {
         alert(`Digite um número entre 0 e 10. Restam ${3-qtd} tentativa(s)`);
     } else if (chute > nSecret){
-        resultado.innerHTML = `Número muito alto...<br>Escolha outro!<br><br>Restam ${3-qtd} tentativa(s)`;
+        resultado.innerHTML = `Número muito alto...<br>Escolha outro!<br>Restam ${3-qtd} tentativa(s)`;
+        chute.value = '' // APAGA O VALOR DIGITADO
     } else if(chute < nSecret){
-        resultado.innerHTML = `Número muito baixo...<br>Escolha outro!<br><br>Restam ${3-qtd} tentativa(s)`;    
+        resultado.innerHTML = `Número muito baixo...<br>Escolha outro!<br>Restam ${3-qtd} tentativa(s)`;    
     }
-
+    
 } while(chute =! nSecret && qtd <3);
 if(qtd >= 3) {
 
     btJN.style.display = 'inline-block';
     btC.style.display = 'none';
     resultado.innerHTML = `=== GAME OVER ===<br>O número secreto é ${nSecret}`;
-}
+} 
 }
