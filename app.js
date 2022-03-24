04da4a0f2921638cc73daaefb601a2992987ce83
Mateus <contatomateusalves@hotmail.com>
2022-03-24 18:18:55 -0300

    Updaten 2.5

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ btJN.style.display = 'none';
 
 // console.log(nSecret)
 
+document.getElementById("valor").disabled = false;
+
 function Jogar() {
     location.reload(); 
 }
@@ -26,21 +28,32 @@ function Chutar() {
         btJN.style.display = 'inline-block';
         btC.style.display = 'none';
         resultado.innerHTML = `=== GAME OVER ===<br>O número secreto é ${nSecret}`;
+        document.getElementById("valor").disabled = true;
+        valor.style.color = "white"
+        // valor.style.border = "none"
     } else  if(chute == nSecret) {
         resultado.innerHTML = `PARABÉNS!<br>Você acertou na ${qtd}ª tentativa.<br>O número secreto é ${nSecret}`;
         btJN.style.display = 'inline-block';
         btC.style.display = 'none';
+        valor.style.color = "white"
+        document.getElementById("valor").disabled = true;
     } else if (chute >10 || chute <0) {
         alert(`Digite um número entre 0 e 10. Restam ${3-qtd} tentativa(s)`);
+        valor.value = "";
+        valor.focus();
     } else if (chute > nSecret){
         resultado.innerHTML = `Número muito alto...<br>Escolha outro!<br>Restam ${3-qtd} tentativa(s)`;
+        valor.value = "";
+        valor.focus();
         chute.value = '' // APAGA O VALOR DIGITADO
     } else if(chute < nSecret){
-        resultado.innerHTML = `Número muito baixo...<br>Escolha outro!<br>Restam ${3-qtd} tentativa(s)`;    
+        resultado.innerHTML = `Número muito baixo...<br>Escolha outro!<br>Restam ${3-qtd} tentativa(s)`;
+        valor.value = "";
+        valor.focus();    
     } 
     
-    valor.value = "";
-    valor.focus();
+    // valor.value = "";
+    // valor.focus();
 
 } while(chute != nSecret && qtd <3);
 } 
